c68a74233dc3ad875ec6f954517d092209bc9a3e
Руслан <agishevrus@gmail.com>
2019-06-19 15:09:22 +0300

    Remove All The Marked Elements of a List

--- CodeWars.UnitTests/KataTests.cs
+++ CodeWars.UnitTests/KataTests.cs
@@ -9,37 +9,46 @@ namespace CodeWars.UnitTests
     public class KataTests
     {
         [Test]
-        public void ExampleTests()
+        public void OrderedCountTest()
         {
             Assert.AreEqual(
                 new List<Tuple<char, int>>() {
-                    tuple('a', 5),
-                    tuple('b', 2),
-                    tuple('r', 2),
-                    tuple('c', 1),
-                    tuple('d', 1)
+                    Tuple('a', 5),
+                    Tuple('b', 2),
+                    Tuple('r', 2),
+                    Tuple('c', 1),
+                    Tuple('d', 1)
                 },
                 Kata.OrderedCount("abracadabra")
             );
             Assert.AreEqual(
                 new List<Tuple<char, int>>() {
-                    tuple('C', 1),
-                    tuple('o', 1),
-                    tuple('d', 1),
-                    tuple('e', 1),
-                    tuple(' ', 1),
-                    tuple('W', 1),
-                    tuple('a', 1),
-                    tuple('r', 1),
-                    tuple('s', 1)
+                    Tuple('C', 1),
+                    Tuple('o', 1),
+                    Tuple('d', 1),
+                    Tuple('e', 1),
+                    Tuple(' ', 1),
+                    Tuple('W', 1),
+                    Tuple('a', 1),
+                    Tuple('r', 1),
+                    Tuple('s', 1)
                 },
                 Kata.OrderedCount("Code Wars")
             );
         }
-
-        private static Tuple<char, int> tuple(char character, int count)
+        private static Tuple<char, int> Tuple(char character, int count)
         {
             return new Tuple<char, int>(character, count);
+        }
+
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 2, 3, 1, 2, 3, 4 }, new int[] { 1, 3 }, ExpectedResult = new int[] { 2, 2, 4 })]
+        [TestCase(new int[] { 1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8 }, new int[] { 1, 3, 4, 2 }, ExpectedResult = new int[] { 5, 6, 7, 8 })]
+        [TestCase(new int[] { }, new int[] { 2, 2, 4, 3 }, ExpectedResult = new int[] { })]
+        public static int[] RemoveTest(int[] integerList, int[] valuesList)
+        {
+            return Kata.Remove(integerList, valuesList);
         }
     }
 }
--- CodeWars/Kata.cs
+++ CodeWars/Kata.cs
@@ -589,6 +589,24 @@ namespace CodeWars
 
         #region <7kyu>
         /// <summary>
+        /// Define a method/function that removes from a given array of integers all the values contained in a second array.
+        /// Examples:
+        /// int[] integerList = new int[] { 1, 1, 2, 3, 1, 2, 3, 4 }
+        /// int[] valuesList = new int[] { 1, 3 }
+        /// Kata.Remove(integerList, valuesList) == new int[]{ 2, 2, 4 } // --> true
+        /// int[] integerList = new int[] { 1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8 }
+        /// int[] valuesList = new int[] { 1, 3, 4, 2 }
+        /// Kata.Remove(integerList, valuesList) == new int[]{ 5, 6 ,7 ,8 } // --> true
+        /// int[] integerList = new int[] { 8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3 }
+        /// int[] valuesList = new int[] { 2, 4, 3 }
+        /// Kata.Remove(integerList, valuesList) == new int[]{ 8, 7, 6, 5, 1 } // --> true
+        /// Enjoy it!!
+        /// </summary>
+        /// <param name="integerList"></param>
+        /// <param name="valuesList"></param>
+        /// <returns></returns>
+        public static int[] Remove(int[] integerList, int[] valuesList) => integerList.Where(x => !valuesList.Contains(x)).ToArray();
+        /// <summary>
         /// Your task
         /// You are given a dictionary/hash/object containing some languages and your test results in the given languages.Return the list of languages where your test score is at least 60, in descending order of the results.
         /// Note: There will be no duplicate values.
