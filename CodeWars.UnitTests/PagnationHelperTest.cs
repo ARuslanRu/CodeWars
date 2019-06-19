55aaa94593930f583ccfbb3b49c089ed9f571efe
Руслан <agishevrus@gmail.com>
2019-06-19 14:33:22 +0300

    Ordered Count of Characters

--- CodeWars.UnitTests/PagnationHelperTest.cs
+++ CodeWars.UnitTests/PagnationHelperTest.cs
@@ -2,8 +2,9 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using CodeWars;
 
-namespace Tests
+namespace CodeWars.UnitTests
 {
+    [TestFixture]
     public class PagnationHelperTest
     {
         private readonly IList<int> collection = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
--- CodeWars/Kata.cs
+++ CodeWars/Kata.cs
@@ -631,5 +631,17 @@ namespace CodeWars
             return Regex.Replace(url, @"#\w*", "");
         }
         #endregion
+
+        #region <8kyu>
+        /// <summary>
+        /// Count the number of occurrences of each character and return it as a list of tuples in order of appearance.
+        /// Example:
+        /// Kata.orderedCount("abracadabra") == new List<Tuple<char, int>>() { new Tuple<char, int>('a', 5), new Tuple<char, int>('b', 2), new Tuple<char, int>('r', 2), new Tuple<char, int>('c', 1), new Tuple<char, int>('d', 1)}
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static List<Tuple<char, int>> OrderedCount(string input) => 
+            input.GroupBy(x => x).Select(x => new Tuple<char, int>(x.Key, x.Count())).ToList();
+        #endregion
     }
 }
