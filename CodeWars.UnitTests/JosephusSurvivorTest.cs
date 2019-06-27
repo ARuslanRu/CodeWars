142c5c49e79588fcffcd5bf5b9340dba20d019b9
Руслан <agishevrus@gmail.com>
2019-06-27 16:18:17 +0300

    Josephus Survivor

---
+++ CodeWars.UnitTests/JosephusSurvivorTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    [TestFixture]
+    public class JosephusSurvivorTest
+    {
+        private static void testing(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests JosSurvivor");
+            testing(Kata.JosSurvivor(7, 3), 4);
+            testing(Kata.JosSurvivor(11, 19), 10);
+            testing(Kata.JosSurvivor(40, 3), 28);
+            testing(Kata.JosSurvivor(14, 2), 13);
+            testing(Kata.JosSurvivor(100, 1), 100);
+            testing(Kata.JosSurvivor(1, 300), 1);
+            testing(Kata.JosSurvivor(2, 300), 1);
+            testing(Kata.JosSurvivor(5, 300), 1);
+            testing(Kata.JosSurvivor(7, 300), 7);
+            testing(Kata.JosSurvivor(300, 300), 265);
+        }
+        //-----------------------
+        private static Random rnd = new Random();
+
+        public static int JosSurvivorSol(int n, int k)
+        {
+            if (n == 1)
+                return 1;
+            return (JosSurvivorSol(n - 1, k) + k - 1) % n + 1;
+        }
+        //-----------------------
+        private static void test2()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                int n = rnd.Next(1, 5000);
+                int k = rnd.Next(1, 5000);
+                testing(Kata.JosSurvivor(n, k), JosSurvivorSol(n, k));
+            }
+        }
+        [Test]
+        public static void RandomTests()
+        {
+            Console.WriteLine("Random Tests******* JosSurvivor");
+            test2();
+        }
+    }
+}
--- CodeWars/Kata.cs
+++ CodeWars/Kata.cs
@@ -75,6 +75,42 @@ namespace CodeWars
 
         #region <5kyu>
         /// <summary>
+        /// In this kata you have to correctly return who is the "survivor", ie: the last element of a Josephus permutation.
+        /// Basically you have to assume that n people are put into a circle and that they are eliminated in steps of k elements, like this:
+        /// josephus_survivor(7,3) => means 7 people in a circle;
+        /// one every 3 is eliminated until one remains
+        /// [1, 2, 3, 4, 5, 6, 7] - initial sequence
+        /// [1, 2, 4, 5, 6, 7] => 3 is counted out
+        /// [1,2,4,5,7] => 6 is counted out
+        /// [1,4,5,7] => 2 is counted out
+        /// [1,4,5] => 7 is counted out
+        /// [1,4] => 5 is counted out
+        /// [4] => 1 counted out, 4 is the last element - the survivor!
+        /// The above link about the "base" kata description will give you a more thorough insight about the origin of this kind of permutation, but basically that's all that there is to know to solve this kata.
+        /// Notes and tips: using the solution to the other kata to check your function may be helpful, but as much larger numbers will be used, using an array/list to compute the number of the survivor may be too slow; you may assume that both n and k will always be >=1.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int JosSurvivor(int n, int k)
+        {
+            if (k == 1) return n;
+            var array = Enumerable.Range(1, n).ToList();
+            int count = 0;
+            while (array.Count() != 1)
+            {
+                var tempArray = new List<int>();
+                for (int i = 0; i < array.Count(); i++)
+                {
+                    count++;
+                    if (count % k != 0) tempArray.Add(array[i]);
+                }
+                array = tempArray;
+            }
+            return array[0];
+        }
+
+        /// <summary>
         /// In this example you have to validate if a user input string is alphanumeric. The given string is not nil, so you don't have to check that.
         /// The string has the following conditions to be alphanumeric:
         /// At least one character("" is not valid)
