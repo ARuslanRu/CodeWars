8752955b43dfebe631d1f1e0b2c627ea64237066
Руслан <agishevrus@gmail.com>
2019-06-27 13:36:29 +0300

    Implement Pagination

---
+++ CodeWars.UnitTests/PaginationTest.cs
@@ -0,0 +1,228 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.UnitTests
+{
+    [TestFixture]
+    class PaginationTest
+    {
+        List<int> values;
+        List<int> random;
+        List<string> books;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            values = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+            random = new List<int>();
+
+            var rnd = new Random();
+            for (int i = 0; i < 45; i++) random.Add(rnd.Next(1, 12345));
+
+            books = new List<string>()
+    {
+      "Watership Down, Richard Adams",
+      "Brave New World, Aldous Huxley",
+      "1984, George Orwell",
+      "Fahrenheit 451, Ray Bradbury",
+      "Non-Violent Resistance: Satyagraha, Mohandas K. Gandhi",
+      "The Wonderful Story of Henry Sugar and Six More, Roald Dahl",
+      "Island, Aldous Huxley",
+      "Green Shadows, White Whale, Ray Bradbury",
+      "In the Penal Colony, Franz Kafka",
+      "The Catcher in the Rye, J. D. Salinger",
+      "The Last Battle, C. S. Lewis",
+      "Borderliners, Peter Hoeg",
+      "Animal Farm, George Orwell",
+      "Our Town, Thornton Wilder",
+      "Beware the Fish!, Gordon Korman",
+      "One Day in the Life of Ivan Denisovich, Alexander Solzhenitsyn",
+      "The Mouse That Roared (series), Leonard Wibberly",
+      "Night, Elie Wiesel",
+      "Cat’s Cradle, Kurt Vonnegut Jr.",
+      "The Short Stories, Ernest Hemingway",
+      "The Great Gatsby, F. Scott Fitzgerald",
+      "Antigone, Sophocles",
+      "I Sing the Body Electric!, Ray Bradbury",
+      "Trout Fishing in America, Richard Brautigan",
+      "All Creatures Great and Small (series), James Herriot",
+      "Babar’s Anniversary Book, Jean and Laurent de Brunhoff",
+      "Pigs Might Fly, Dick King-Smith",
+      "James and the Giant Peach, Roald Dahl",
+      "The Jungle Book, Rudyard Kipling",
+      "Gandhi on Non-Violence, Mohandas K. Gandhi",
+      "The Little History of the Wide World, Mable Pyne"
+    };
+        }
+
+        [Test]
+        public void ValuesPage1Test()
+        {
+            var p = new Pagination<int>(values);
+
+            Assert.AreEqual(1, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(23, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(3, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void ValuesPage2Test()
+        {
+            var p = new Pagination<int>(values);
+
+            p.CurrentPage = 2;
+
+            Assert.AreEqual(2, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(23, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(3, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(new List<int>() { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 }),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void ValuesPage3Test()
+        {
+            var p = new Pagination<int>(values);
+
+            p.CurrentPage = 3;
+
+            Assert.AreEqual(3, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(23, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(3, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(new List<int>() { 21, 22, 23 }),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void BooksPage1Test()
+        {
+            var p = new Pagination<string>(books);
+
+            p.CurrentPage = 1;
+            p.ItemsPerPage = 5;
+
+            Assert.AreEqual(1, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(31, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(7, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(books.GetRange(0, 5)),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void BooksPage2Test()
+        {
+            var p = new Pagination<string>(books);
+
+            p.CurrentPage = 2;
+            p.ItemsPerPage = 5;
+
+            Assert.AreEqual(2, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(31, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(7, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(books.GetRange(5, 5)),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void BooksPage3Test()
+        {
+            var p = new Pagination<string>(books);
+
+            p.CurrentPage = 3;
+            p.ItemsPerPage = 5;
+
+            Assert.AreEqual(3, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(31, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(7, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(books.GetRange(10, 5)),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            var p = new Pagination<int>(random);
+
+            var rnd = new Random();
+
+            int items = rnd.Next(1, random.Count);
+            int totalpages = (int)Math.Ceiling(random.Count / (decimal)items);
+            int page = rnd.Next(1, totalpages);
+
+            p.CurrentPage = page;
+            p.ItemsPerPage = items;
+
+            Assert.AreEqual(page, p.CurrentPage, "What page are you on? (CurrentPage)");
+            Assert.AreEqual(random.Count, p.Total, "Did you lose something? (Total)");
+            Assert.AreEqual(totalpages, p.TotalPages, "Hmmm? (TotalPages)");
+
+            Assert.IsTrue(
+              p.Items.SequenceEqual(random.GetRange((page - 1) * items, items)),
+              "Yeah, I dont think we are on the same page (Items)"
+            );
+        }
+
+        [Test]
+        public void GettableSettable()
+        {
+            Assert.IsFalse(
+              typeof(Pagination<string>).GetProperty("Items").CanWrite
+              || typeof(Pagination<string>).GetProperty("Total").CanWrite
+              || typeof(Pagination<string>).GetProperty("TotalPages").CanWrite,
+              "Properties Items, Total and TotalPages should only be gettable"
+            );
+        }
+
+        [Test]
+        public void DefaultPropertyValues()
+        {
+            var p = new Pagination<int>(values);
+
+            Assert.AreEqual(1, p.CurrentPage, "Default value incorrect (CurrentPage)");
+            Assert.AreEqual(10, p.ItemsPerPage, "Default value incorrect (ItemsPerPage)");
+        }
+
+        [Test]
+        public void InvalidPropertyValues()
+        {
+            var p = new Pagination<int>(values);
+
+            p.CurrentPage = -1;
+            Assert.AreEqual(1, p.CurrentPage, "Property should use default if set to invalid (CurrentPage)");
+
+            p.ItemsPerPage = -1;
+            Assert.AreEqual(10, p.ItemsPerPage, "Property should use default if set to invalid (ItemsPerPage)");
+        }
+
+        [Test]
+        public void EmptyOnHigherThanTotalPages()
+        {
+            var p = new Pagination<int>(values);
+
+            p.CurrentPage = p.TotalPages + 1;
+            Assert.IsTrue(p.Items.Count() == 0, "Items should be empty if CurrentPage > TotalPages");
+        }
+    }
+}
---
+++ CodeWars/Pagination.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    /// <summary>
+    /// Pagination is about dividing up results into pages with a fixed number of items on each page, your task is to write a generic pagination class that manages pagination for any class implementing IEnumerable<T>
+    /// The class should take in the IEnumerable<T> to manage in the constructor and must have the following properties:
+    /// IEnumerable<T> Items // gets the items for the current page index
+    /// int CurrentPage      // gets/sets the current page index, starting at 1
+    /// int ItemsPerPage     // gets/sets the number of items to return on each page, default 10 items
+    /// int Total            // gets the total number of items in the source
+    /// int TotalPages       // gets total number of pages
+    /// If CurrentPage is set to <= 0 it should default back to page 1.
+    /// If CurrentPage is set to > TotalPages Items should be empty (not null).
+    /// If ItemsPerPage is set to <= 0 it should default back to 10 items.
+    /// Properties listed as only gettable should not be settable.
+    /// Settable properties can be set in any order.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Pagination<T>
+    {
+        IEnumerable<T> _source;
+        int _currentPage = 1;
+        int _itemsPerPage = 10;
+
+        // gets the items for the current page index
+        public IEnumerable<T> Items
+        {
+            get
+            {
+                return _source.Skip((CurrentPage - 1) * ItemsPerPage).Take(ItemsPerPage);
+            }
+        }
+
+        // gets/sets the current page index, starting at 1
+        public int CurrentPage
+        {
+            get
+            {
+                return _currentPage;
+            }
+            set
+            {
+                _currentPage = value <= 0 ? 1 : value;
+            }
+        }
+
+        // gets/sets the number of items to return on each page, default 10 items
+        public int ItemsPerPage 
+        {
+            get
+            {
+                return _itemsPerPage;
+            }
+            set
+            {
+                _itemsPerPage = value <= 0 ? 10 : value;
+            }
+        }
+
+        // gets the total number of items in the source
+        public int Total { get; }
+        
+        // gets total number of pages
+        public int TotalPages 
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)Total / ItemsPerPage);
+            }
+        }
+        public Pagination(IEnumerable<T> source)
+        {
+            _source = source;
+            Total = source.Count();
+        }
+    }
+}
